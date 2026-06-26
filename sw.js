5f39d95b643768650cd9f069ee0493656889982d
thanwarat345 <68301430004@pbtc.ac.th>
2026-06-26 19:51:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,20 +6,29 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
+// ฟังก์ชัน fetch แบบพื้นฐาน ให้เบราว์เซอร์มองว่าเป็น PWA ที่สมบูรณ์
 self.addEventListener('fetch', (event) => {
-  // ตอบกลับแบบพื้นฐาน เพื่อให้เบราว์เซอร์มองว่ามีระบบ Offline จัดการอยู่
   event.respondWith(
     fetch(event.request).catch(() => new Response('กรุณาเชื่อมต่ออินเทอร์เน็ต'))
   );
 });
 
-// จัดการเรื่องแจ้งเตือน
+// จัดการเมื่อผู้ใช้กดที่การแจ้งเตือน
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
     clients.matchAll({ type: 'window' }).then(windowClients => {
-      if (windowClients.length > 0) return windowClients[0].focus();
-      return clients.openWindow('/');
+      // ถ้าเปิดแอปไว้อยู่แล้ว ให้เด้งแอปขึ้นมา
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url.includes('index.html') && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      // ถ้าไม่ได้เปิดแอปไว้ ให้เปิดหน้าต่างใหม่
+      if (clients.openWindow) {
+        return clients.openWindow('./index.html');
+      }
     })
   );
 });
